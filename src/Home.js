551d46d89453362e5d71befe8addb38d38165a5c
PR0C0S <prayagrajghimire@gmail.com>
2023-12-04 21:35:58 +0545

    comms

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,40 @@ function Home(){
         <div className="mainPic">
         <img src={picMain} alt="Section 1 Image"></img>
         </div>
+
+        <div className="yellow-background">
+                <div className="icon-background">
+                    <div className="panels">
+                        
+                    <div class="panel-content">
+                            <img src={pan2} alt="Image 4"/>
+                        </div>
+                    </div>
+                    <div className="panels">
+                        <div class="panel-content">
+                            <img src={pan1} alt="Image 4"/>
+                        </div>
+                    </div>
+                    <div className="panels">
+                        <div class="panel-content">
+                            <img src={pan3} alt="Image 4"/>
+                        </div>
+                    </div>
+                    <div className="panels">
+                        <div class="panel-content">
+                            <img src={pan5} alt="Image 4"/>
+                        </div>
+                    </div>
+                    <div className="panels">
+                        <div class="panel-content">
+                            <img src={pan4} alt="Image 5"/>
+                        </div>
+                    </div>
+    </div>
+</div>
+
+
+
         <div className="sub-div">
             <div className="section">
                 <div className="img-section">
